aeefacf44ebc45f09c5f3ef0db5a1e742e965fcc
liangxu <lshaci@qq.com>
2019-07-30 18:11:23 +0800

    <feat>callableDemo

---
+++ interview/src/main/java/com/lshaci/interview/thread/pool/CallableDemo.java
@@ -0,0 +1,28 @@
+package com.lshaci.interview.thread.pool;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<Integer> futureTask = new FutureTask<>(new MyCallable());
+
+        new Thread(futureTask, "F").start();
+        new Thread(futureTask, "T").start(); // 不会再次进入call方法
+
+        Integer r = futureTask.get(); // 会阻塞
+
+        System.err.println("=========>结果：" + r);
+    }
+}
+
+class MyCallable implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        System.err.println("***************call();");
+        return 1024;
+    }
+}
